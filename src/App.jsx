290a91f4b2ae3cc65049fb8495e5c43df7913ea2
Gloria <hanning_hit@hotmail.com>
2022-05-30 12:58:20 +1000

    add comment in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import Footer from './components/Footer'
 import Header from'./components/Header'
 import List from './components/List'
-
+//暴露 App
 export default class App extends Component {
     state = {
         todos:[
